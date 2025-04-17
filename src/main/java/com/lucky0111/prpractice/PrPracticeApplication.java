b1c0ee602978ef1e7f9b41d9a446410c37360f58
김주엽 <kjyy08@naver.com>
2025-04-17 11:29:35 +0900

    first commit

---
+++ src/main/java/com/lucky0111/prpractice/PrPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.lucky0111.prpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrPracticeApplication.class, args);
+    }
+
+}
