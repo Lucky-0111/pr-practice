abf042daddb896dbf62add6643238987d46eebe6
김주엽 <kjyy08@naver.com>
2025-04-17 20:19:54 +0900

    이제 슬슬 커밋 귀찮

--- src/main/java/com/lucky0111/prpractice/controller/HomeController.java
+++ src/main/java/com/lucky0111/prpractice/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public ResponseEntity<?> home() {
-        return ResponseEntity.ok("Hello Test 5");
+        return ResponseEntity.ok("Hello Test 6");
     }
 }
