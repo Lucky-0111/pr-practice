9e10b6815ba4af9e09ec73d592336a8fe8af3fe2
김주엽 <kjyy08@naver.com>
2025-04-17 17:06:00 +0900

    feat: ci/cd 테스트

--- src/main/java/com/lucky0111/prpractice/entity/User.java
+++ src/main/java/com/lucky0111/prpractice/entity/User.java
@@ -8,7 +8,6 @@ public class User {
     private String username;
     private String email;
 
-
     public User() {}
     public User(String username, String email) {
         this.username = username;
