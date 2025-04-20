5266ddfadad3ead40990e82036c78954376ab0eb
김주엽 <kjyy08@naver.com>
2025-04-20 20:34:34 +0900

    테스트 강제 실패

--- src/main/java/com/lucky0111/prpractice/controller/HomeController.java
+++ src/main/java/com/lucky0111/prpractice/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public ResponseEntity<?> home() {
-        return ResponseEntity.ok("슬랙테스트");
+        return ResponseEntity.notFound().build();
     }
 }
